37d3e43a9c61e2f3a3bd421e535a31e1030fde63
Santiago Lamot <santiagolamot@hotmail.com>
2025-08-30 14:24:50 -0300

    Update index.tsx

--- portal Frontend/src/app/routes/index.tsx
+++ portal Frontend/src/app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate, ServerRouter } from "react-router-dom";
 
 // Importá las páginas que quieras usar en cada ruta
 import MainLayout from "../../shared/layouts/MainLayouts";
@@ -6,6 +6,8 @@ import HomeRoutes from "../../features/home/routes";
 import ComercioRoutes from "../../features/comercio/routes";
 import GastronomiaRoutes from "../../features/gastronomia/routes";
 import HospedajeRoutes from "../../features/hospedaje/routes";
+import EsPublicosRoutes from "../../features/espacios publicos/routes";
+import ServiciosRoutes from "../../features/servicios/routes";
 import TermasRoutes from "../../features/termas/routes";
 
 export const router = createBrowserRouter([
@@ -14,10 +16,12 @@ export const router = createBrowserRouter([
         element: <MainLayout />,
         children: [
             { path: "/", element: <HomeRoutes /> },
+            { path: "/termas/*", element: <TermasRoutes /> },
             { path: "/comercio/*", element: <ComercioRoutes /> },
             { path: "/gastronomia/*", element: <GastronomiaRoutes /> },
             { path: "/hospedaje/*", element: <HospedajeRoutes /> },
-            { path: "/termas/*", element: <TermasRoutes /> },
+            { path: "/servicios/*", element: < ServiciosRoutes/> },
+            { path: "/espublicos/*", element: <EsPublicosRoutes /> },
             // Ruta comodín: si no existe redirige al inicio
             { path: "*", element: <Navigate to="/" /> },
         ],
