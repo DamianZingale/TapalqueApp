3ac6f888a69f347c43c10e3b2bf26ce2df8944ef
Santiago Lamot <santiagolamot@hotmail.com>
2025-08-31 12:45:01 -0300

    Update ICardProps.ts

--- portal Frontend/src/shared/types/ICardProps.ts
+++ portal Frontend/src/shared/types/ICardProps.ts
@@ -1,5 +1,8 @@
+import { type SECCION_TYPE } from "../constants/constSecciones";
+
 export interface CardProps {
     id: string;
     titulo: string;
     imagenUrl: string;
+    tipo: typeof SECCION_TYPE[keyof typeof SECCION_TYPE];
 }
