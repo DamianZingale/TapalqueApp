8e6f1bd7c12fd9f1731f2ede1e9dc515ae5b2955
Santiago Lamot <santiagolamot@hotmail.com>
2025-08-31 12:44:56 -0300

    Create constSecciones.ts

---
+++ portal Frontend/src/shared/constants/constSecciones.ts
@@ -0,0 +1,7 @@
+export const SECCION_TYPE = {
+    COMERCIO: 'comercio',
+    GASTRONOMIA: 'gastronomia',
+    HOSPEDAJES: 'hospedaje',
+    SERVICIOS: 'servicios',
+    ESP_PUBLICOS: 'espublicos'
+} as const
