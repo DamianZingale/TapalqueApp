e0f8c5cef221eee994b7497ecd04673e4b6fba3b
Santiago Lamot <santiagolamot@hotmail.com>
2025-08-31 12:44:36 -0300

    Update HomePage.tsx

--- portal Frontend/src/features/home/pages/HomePage.tsx
+++ portal Frontend/src/features/home/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import { CardGridResponsive } from "../components/CardsResponsives"
 import { CardTermas } from "../components/CardTermas"
+import TapalqueWeather from "../components/TapalqueWeather";
 
 export const HomePage = () => {
   return (
-    <>
-    <CardTermas/>
-    <CardGridResponsive/>
-    </>
-  )
-}
+    <div className="container">
+      <div className="row justify-content-center mb-4">
+        <div className="col-12 col-sm-10 col-md-10 col-lg-12">
+          <CardTermas />
+        </div>
+      </div>
+      <CardGridResponsive />
+      <TapalqueWeather/>
+    </div>
+  );
+};
