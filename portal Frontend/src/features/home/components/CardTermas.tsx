caf4dbd43a9b764e33a7c9da3cb02cb9a002586d
Santiago Lamot <santiagolamot@hotmail.com>
2025-08-31 12:44:31 -0300

    Update CardTermas.tsx

--- portal Frontend/src/features/home/components/CardTermas.tsx
+++ portal Frontend/src/features/home/components/CardTermas.tsx
@@ -1,32 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import { CardBackground } from "./CardBackground";
 
 export const CardTermas = () => {
-    
     const navigate = useNavigate();
-    const titulo = "TERMAS";
-    const imagenUrl = "https://termastapalque.com.ar/wp-content/uploads/2023/08/piletas-02-b.webp";
-    const destino = "/termas"; // ruta a la que navega
     return (
-        <div
-            className="card text-white mb-4 "
-            style={{
-                backgroundImage: `url(${imagenUrl})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                height: "170px",
-                cursor: "pointer",
-                position: "relative",
-                borderRadius: "0.5rem",
-                overflow: "hidden",
-            }}
-            onClick={() => navigate(destino)}
-        >
-            <div
-                className="card-img-overlay d-flex flex-column justify-content-center align-items-center"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.45)" }}
-            >
-                <h2 className="fw-bold" style={{ letterSpacing: "1.60em" }}>{titulo}</h2>
-            </div>
-        </div>
+        <CardBackground
+            titulo="TERMAS"
+            imagenUrl="https://termastapalque.com.ar/wp-content/uploads/2023/08/piletas-02-b.webp"
+            onClick={() => navigate("/termas")}
+            overlayOpacity={0.45}
+            letterSpacing="1.6em"
+            fontSize="1.5rem"
+        />
     );
 };
