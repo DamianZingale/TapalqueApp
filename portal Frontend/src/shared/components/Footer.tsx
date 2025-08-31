1d9a70bdf1bc8c30bcc8061fa1e285134e1269b2
Santiago Lamot <santiagolamot@hotmail.com>
2025-08-31 12:44:54 -0300

    Create Footer.tsx

---
+++ portal Frontend/src/shared/components/Footer.tsx
@@ -0,0 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+    return (
+        <footer className="bg-dark text-light py-4 mt-auto">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6} className="text-center text-md-end">
+                        <Link to="#" className="text-light text-decoration-none me-3">Nosotros</Link>
+                        <Link to="#" className="text-light text-decoration-none me-3">Terminos y condiciones</Link>
+                    </Col>
+                    <Col md={6} className="text-center text-md-start mb-2 mb-md-0">
+                        <small>Desarrollado por <a className="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/in/damian-zingale-7a89bb114/">Damian Zingale</a> y <a className="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/in/santiago-lamot-/">Santiago Lamot</a>.</small>
+                        <br/>
+                        <small>&copy; {new Date().getFullYear()} Tapalqué App. Todos los derechos reservados.</small>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
