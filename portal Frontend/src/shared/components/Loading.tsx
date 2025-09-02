9934961acdf6f75ebfaf188f6078659cd849f6ae
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-02 16:36:29 -0300

    Create Loading.tsx

---
+++ portal Frontend/src/shared/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { type LoadingProps } from "../types/ui/LoadingProps"
+
+export const Loading :React.FC<LoadingProps>= ({text}) => {
+    return (
+        <div className="d-flex flex-column align-items-center justify-content-center py-5 text-muted">
+            <div className="spinner-border text-secundary mb-3" role="status" />
+            <span>{text}</span>
+        </div>
+    )
+}
