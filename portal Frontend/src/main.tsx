a2f02290f4e5e45e64905d868b2cc9a5c744a82a
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-08 19:44:37 -0300

    Update main.tsx

--- portal Frontend/src/main.tsx
+++ portal Frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
