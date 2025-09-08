5e0b82c5cf7617f0662f626ebc80f2077e3afbe2
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-08 16:47:45 -0300

    Create Horarios.tsx

---
+++ portal Frontend/src/shared/components/Horarios.tsx
@@ -0,0 +1,19 @@
+import type { HorariosProps } from "../types/ui/ComercioProps"
+
+export const Horarios : React.FC<HorariosProps>= ({horarios}) => {
+    return (
+        <p
+            style={{
+                whiteSpace: "pre-line",
+                textAlign: "justify",
+                fontSize: "1.1rem",
+                lineHeight: "1.6",
+                color: "#444",
+                fontWeight: 400,
+                marginBottom: "1.5rem",
+            }}
+        >
+            {`Nuestros horarios:\n${horarios}`}
+        </p>
+    )
+}
