21bd5e8904a9304c6a63bace93a9dc577a366439
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-08 15:35:43 -0300

    Update ComercioDetailsPages.tsx

--- portal Frontend/src/features/comercio/pages/ComercioDetailsPages.tsx
+++ portal Frontend/src/features/comercio/pages/ComercioDetailsPages.tsx
@@ -1,6 +1,21 @@
 import { useParams } from "react-router-dom";
+import { Title } from "../../../shared/components/Title";
+import { Carrusel } from "../../../shared/components/Carrusel";
 
 export default function ComercioDetailPage() {
     const { id } = useParams();
-    return <h1>Detalle del Comercio {id}</h1>;
+    const data = {
+        nombre : "Comercio General Prueba",
+        imagenes : [
+            "https://i.pinimg.com/originals/0e/b3/bb/0eb3bb977428d9433ca07741706e83ae.jpg",
+            "https://st2.depositphotos.com/1003697/8297/i/450/depositphotos_82978822-stock-photo-supermarket-store-with-vegetables.jpg"
+        ]
+    }
+    return (
+        <div className="container">
+            <Title text={`${data.nombre} ${id}`}/>
+            <Carrusel images={data.imagenes}/>
+        </div>
+    )
+    ;
 }
