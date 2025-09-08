2f6c28cbf92ef543bf2f1999a74c6b5eedaae5be
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-08 15:00:41 -0300

    Create Title.tsx

---
+++ portal Frontend/src/shared/components/Title.tsx
@@ -0,0 +1,9 @@
+import type { TittleProps } from "../types/ui/ComercioProps"
+
+export const Title : React.FC<TittleProps>= ({text}) => {
+    return (
+        <h2 className="text-center my-4">
+            {text}
+        </h2>
+    )
+}
