0ffde1aa629a1af42b56bcba9dcc953c30c52e99
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-11 19:49:27 -0300

    Update routes.tsx

--- portal Frontend/src/features/perfil/routes.tsx
+++ portal Frontend/src/features/perfil/routes.tsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { PerfilPage } from "./pages/PerfilPage";
+import { DatosPersonalesPage } from "./pages/DatosPersonalesPage";
+import { NotificacionesPages } from "./pages/NotificacionesPages";
+import { MisPedidosPage } from "./pages/MisPedidosPage";
+import { MisReservasPage } from "./pages/MisReservasPage";
 
 export default function PerfilRoutes() {
     return (
         <Routes>
             <Route path="/" element={<PerfilPage />} />
+            <Route path="/datosPersonales" element={<DatosPersonalesPage />} />
+            <Route path="/notificaciones" element={<NotificacionesPages />} />
+            <Route path="/misPedidos" element={<MisPedidosPage />} />
+            <Route path="/misReservas" element={<MisReservasPage />} />
         </Routes>
     );
 }
