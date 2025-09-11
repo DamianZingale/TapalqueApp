0550b127b021b25f7d1ad1c9c6fbdb37ae39536f
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-11 19:49:17 -0300

    Create MisPedidosPage.tsx

---
+++ portal Frontend/src/features/perfil/pages/MisPedidosPage.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../../../shared/components/Title"
+
+export const MisPedidosPage = () => {
+    return (
+        <div className="container vh-100 flex-column">
+            <Title text="Mis Pedidos" />
+        </div>
+    )
+}
