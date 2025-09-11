83dcb6a325f8acf6d670f8ccab85a2c2259b8e6b
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-11 19:49:22 -0300

    Create NotificacionesPages.tsx

---
+++ portal Frontend/src/features/perfil/pages/NotificacionesPages.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../../../shared/components/Title"
+
+export const NotificacionesPages = () => {
+    return (
+        <div className="container vh-100 flex-column">
+            <Title text="Notificaciones" />
+        </div>
+    )
+}
