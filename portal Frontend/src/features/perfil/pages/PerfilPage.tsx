d98c65a1a30fff7e00de28bfa4a2a590a5e2650d
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-11 19:49:24 -0300

    Update PerfilPage.tsx

--- portal Frontend/src/features/perfil/pages/PerfilPage.tsx
+++ portal Frontend/src/features/perfil/pages/PerfilPage.tsx
@@ -1,5 +1,17 @@
+import { Title } from "../../../shared/components/Title";
+import { BotonOpciones } from "../components/BotonOpciones";
+
 export const PerfilPage = () => {
+
     return (
-        <div>PerfilPage</div>
+        <div className="vh-100 flex-column">
+            <Title text="Mi perfil" />
+            <div className="d-grid gap-2 col-6 mx-auto">
+                <BotonOpciones texto="Datos Personales" redireccion="datosPersonales"/>
+                <BotonOpciones texto="Notificaciones" redireccion="notificaciones"/>
+                <BotonOpciones texto="Mis Pedidos" redireccion="misPedidos"/>
+                <BotonOpciones texto="Mis Reservas" redireccion="misReservas"/>
+            </div>
+        </div>
     )
 }
