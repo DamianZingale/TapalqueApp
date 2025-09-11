66547c3a09a215088a64533168024cd67d221cef
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-11 19:49:14 -0300

    Create DatosPersonalesPage.tsx

---
+++ portal Frontend/src/features/perfil/pages/DatosPersonalesPage.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../../../shared/components/Title"
+
+export const DatosPersonalesPage = () => {
+    return (
+        <div className="container vh-100 flex-column">
+            <Title text= "Datos Personales"/>
+        </div>
+    )
+}
