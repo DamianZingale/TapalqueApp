54de5551574fa8c5617165966d2d560f271c1d51
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-12 16:20:36 -0300

    Update PropsPerfil.ts

--- portal Frontend/src/shared/types/PropsPerfil.ts
+++ portal Frontend/src/shared/types/PropsPerfil.ts
@@ -2,3 +2,17 @@ export interface IBotonOpciones{
     texto: string;
     redireccion: string;
 }
+
+export interface PropsNotificacion{
+    id: string;
+    asunto: string;
+    fecha: string;
+    descripcion: string;
+}
+
+export interface PropsListado{
+    id: string;
+    estado: string;
+    titulo: string;
+    fecha: string;
+}
