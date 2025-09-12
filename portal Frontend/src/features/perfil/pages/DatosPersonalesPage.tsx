90b1e64a1cfd63766c0cfdeb6cfdc4981eaa3eb5
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-12 16:48:58 -0300

    Update DatosPersonalesPage.tsx

--- portal Frontend/src/features/perfil/pages/DatosPersonalesPage.tsx
+++ portal Frontend/src/features/perfil/pages/DatosPersonalesPage.tsx
@@ -1,9 +1,13 @@
 import { Title } from "../../../shared/components/Title"
+import { CambiarContraseña } from "../components/CambiarContrasenia"
+import { DatosPersonales } from "../components/DatosPersonales"
 
 export const DatosPersonalesPage = () => {
     return (
         <div className="container vh-100 flex-column">
             <Title text= "Datos Personales"/>
+            <DatosPersonales/>
+            <CambiarContraseña/>
         </div>
     )
 }
