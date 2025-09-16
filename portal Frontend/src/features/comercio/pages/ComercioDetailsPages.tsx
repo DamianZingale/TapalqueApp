f2ee7accc3ffdf580a5a900633334308e16a591a
Nahuel C. <nahuelncejas@gmail.com>
2025-09-16 16:28:44 -0300

    Avance con el comercio

--- portal Frontend/src/features/comercio/pages/ComercioDetailsPages.tsx
+++ portal Frontend/src/features/comercio/pages/ComercioDetailsPages.tsx
@@ -5,29 +5,22 @@ import { Description } from "../../../shared/components/Description";
 import { GMaps } from "../../../shared/components/GMaps";
 import { Horarios } from "../../../shared/components/Horarios";
 import { WhatsAppButton } from "../../../shared/components/WhatsAppButton";
+import { comerciosMock } from './mocks/mockComercios';
 
 export default function ComercioDetailPage() {
     const { id } = useParams();
-    const data = {
-        nombre: "Comercio General Prueba",
-        imagenes: [
-            "https://i.pinimg.com/originals/0e/b3/bb/0eb3bb977428d9433ca07741706e83ae.jpg",
-            "https://st2.depositphotos.com/1003697/8297/i/450/depositphotos_82978822-stock-photo-supermarket-store-with-vegetables.jpg"
-        ],
-        descripcion: "Tapalqué es un lugar único.\nAquí podés disfrutar de la naturaleza,\nlos deportes y la tranquilidad.\n\nIdeal para escapadas de fin de semana.",
-        urlMaps: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d200.82788778722778!2d-60.025677620491216!3d-36.35481487873783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9595dd3c51921f95%3A0xe615d6e6f38db6f9!2sMinimercado%20Yesi!5e0!3m2!1ses-419!2sar!4v1757358563713!5m2!1ses-419!2sar",
-        horarios: "Lunes a Sabados\nde 08:00hs a 20:00hs.",
-        num: "2281683888"
-    }
+    const data = comerciosMock.find((comercio) => comercio.id === id);
+
+    if (!data) return <p>Comercio no encontrado</p>;
 
     return (
         <div className="container">
-            <Title text={`${data.nombre} ${id}`} />
-            <Carrusel images={data.imagenes} />
-            <Description description={data.descripcion}/>
-            <Horarios horarios={data.horarios}/>
-            <GMaps url={data.urlMaps}/>
-            <WhatsAppButton num={data.num}/>
+        <Title text={data.titulo} />
+        <Carrusel images={data.imagenes} />
+        <Description description={data.descripcion} />
+        <Horarios horarios={data.horarios} />
+        <GMaps url={data.urlMaps} />
+        <WhatsAppButton num={data.num} />
         </div>
-    )
+    );
 }
--- portal Frontend/src/features/comercio/pages/ComercioListPages.tsx
+++ portal Frontend/src/features/comercio/pages/ComercioListPages.tsx
@@ -1,18 +1,24 @@
 import { Card } from "../../../shared/components/Card";
-import {  SECCION_TYPE } from "../../../shared/constants/constSecciones";
+import { SECCION_TYPE } from "../../../shared/constants/constSecciones";
+import { comerciosMock } from './mocks/mockComercios'
 
 export default function ComercioListPage() {
     return (
         <div className="container">
-            <h1 className="text-center my-4">Comercios</h1>
-            <div className="row justify-content-center">
-                <Card
-                    id="cordoba-123"
-                    titulo="Hospedaje en Córdoba"
-                    imagenUrl="https://ejemplo.com/img/cordoba.jpg" 
-                    tipo= {SECCION_TYPE.COMERCIO}
-                    />
-            </div>
+        <h1 className="text-center my-4">Comercios</h1>
+        <div className="row justify-content-center">
+            {comerciosMock.map((comercio) => (
+            <Card
+                key={comercio.id}
+                id={comercio.id}
+                titulo={comercio.titulo}
+                imagenUrl={comercio.imagenUrl}
+                direccion_local={comercio.direccion}
+                tipo={SECCION_TYPE.COMERCIO}
+            />
+
+            ))}
+        </div>
         </div>
     );
 }
