f23331341e855602d045e77ee4287d2ee50e5b4b
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-16 00:14:37 -0300

    Update routes.tsx

--- portal Frontend/src/features/hospedaje/routes.tsx
+++ portal Frontend/src/features/hospedaje/routes.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import HospedajeListPage from "./pages/HospedajeListPage";
 import HospedajeDetailPage from "./pages/HospedajeDetailPage";
-import { ReservaDetailPage } from "./pages/ReservaDetailPage";
+import { HospedajeListsOptions } from "./pages/HospedajeListsOptions";
+import { ConfirmacionPage } from "./pages/ConfirmacionPage";
 
 export default function HospedajeRoutes() {
     return (
         <Routes>
             <Route path="/" element={<HospedajeListPage />} />
             <Route path=":id" element={<HospedajeDetailPage />} />
-            <Route path=":id/reserva/:fecha" element={<ReservaDetailPage />} />
+            <Route path="opciones" element={<HospedajeListsOptions />} />
+            <Route path="confirmar" element={<ConfirmacionPage />} />
         </Routes>
     );
 }
