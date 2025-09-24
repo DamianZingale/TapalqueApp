46ef5caa6de7d19043afce65aa0fd888756b333e
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-23 21:41:18 -0300

    Update AdminGralComerciosPage.tsx

--- portal Frontend/src/features/administrador general/pages/AdminGralComerciosPage.tsx
+++ portal Frontend/src/features/administrador general/pages/AdminGralComerciosPage.tsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import { Title } from "../../../shared/components/Title"
+import { ListadoLocales } from "../components/ListadoLocales"
+import { BotonesAccionAdmin } from "../components/BotonesAccionAdmin";
+
 export const AdminGralComerciosPage = () => {
+    const [selectedId, setSelectedId] = useState<string | null>(null);
+    const [locales] = useState([
+        {
+            id: "1",
+            estado: "Activo",
+            nombre: "La Clandestina",
+            direccion: "Complejo termal de Tapalqué",
+        },
+        {
+            id: "2",
+            estado: "Inactivo",
+            nombre: "La Clandestina",
+            direccion: "Av. Hipolito Yrigoyen N°123",
+        },
+    ]);
+
+    const selectedLocal = locales.find((local) => local.id === selectedId);
+
     return (
-        <div>AdminGralComercios</div>
+        <>
+            <Title text="Comercios" />
+
+            {selectedLocal && (
+                <BotonesAccionAdmin estado={selectedLocal.estado} />)}
+
+            {locales.map((local) => (
+                <ListadoLocales
+                    key={local.id}
+                    {...local}
+                    onSelect={(id) => setSelectedId(id)}
+                    selectedId={selectedId}
+                />
+            ))}
+        </>
     )
 }
