6efa861d2d00041e292d2be6efec417c1d7f3e3b
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-23 22:28:15 -0300

    Update BotonesAccionAdmin.tsx

--- portal Frontend/src/features/administrador general/components/BotonesAccionAdmin.tsx
+++ portal Frontend/src/features/administrador general/components/BotonesAccionAdmin.tsx
@@ -1,12 +1,19 @@
-import type { BotonesAccionProps } from "../../../shared/types/PropsListadoLocales"
+import { useNavigate } from "react-router-dom"
+import type { BotonesAccionProps } from "../../../shared/types/PropsAdminGeneral"
 
 export const BotonesAccionAdmin: React.FC<BotonesAccionProps> = ({estado}) => {
+    const navigate = useNavigate();
+    //Aca falta llamo al backend para eliminar y desacticar el seleccionado.
+    const handleClickEditar = () =>{
+        navigate("editar")
+    }
+
     return (
         <div className="d-flex gap-2 justify-content-center mb-3">
             <button className={`btn ${estado === "Activo" ? "btn-warning" : "btn-success"}`}>
                 {estado === "Activo" ? "Desactivar" : "Activar"}
             </button>
-            <button className="btn btn-primary">Editar</button>
+            <button onClick={handleClickEditar}className="btn btn-primary">Editar</button>
             <button className="btn btn-danger">Eliminar</button>
         </div>
     )
