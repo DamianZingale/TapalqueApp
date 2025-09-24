a9d7ea3e30ddd85be8fd08b0c076d9091d58a910
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-23 21:41:22 -0300

    Create ListadoLocales.tsx

---
+++ portal Frontend/src/features/administrador general/components/ListadoLocales.tsx
@@ -0,0 +1,16 @@
+import type { PropsListadoLocales } from "../../../shared/types/PropsListadoLocales";
+
+export const ListadoLocales: React.FC<PropsListadoLocales> = ({ id, estado, nombre, direccion, onSelect, selectedId }) => {
+    return (
+        <div
+            className={`card text-center mb-4 cursor-pointer ${selectedId === id ? 'border-secondary' : ''}`}
+            onClick={() => onSelect?.(id)}
+        >
+            <div className="card-header">{estado}</div>
+            <div className="card-body">
+                <h5 className="card-title">{nombre}</h5>
+            </div>
+            <div className="card-footer text-body-secondary">{direccion}</div>
+        </div>
+    );
+}
