d57cc7ecbd5da8b8f9e23230505b133bb5d2c73e
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-25 20:38:25 -0300

    Update AdminGralGastronomicosPage.tsx

--- portal Frontend/src/features/administrador general/pages/AdminGralGastronomicosPage.tsx
+++ portal Frontend/src/features/administrador general/pages/AdminGralGastronomicosPage.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import { Title } from "../../../shared/components/Title"
+import { BotonAgregar } from "../components/BotonAgregar"
+import { BotonesAccionAdmin } from "../components/BotonesAccionAdmin"
+import { ListadoLocales } from "../components/ListadoLocales";
 
 export const AdminGralGastronomicosPage = () => {
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [locales] = useState([
+    {
+      id: "1",
+      estado: "Activo",
+      nombre: "La Clandestina",
+      direccion: "Complejo termal de Tapalqué",
+    },
+    {
+      id: "2",
+      estado: "Inactivo",
+      nombre: "La Clandestina",
+      direccion: "Av. Hipolito Yrigoyen N°123",
+    },
+  ]);
+  const selectedLocal = locales.find((local) => local.id === selectedId);
+  
   return (
-    <Title text="Gastronomicos" />
+    <>
+      <Title text="Gastronomicos" />
+      <BotonAgregar />
+      {selectedLocal && (
+        <BotonesAccionAdmin estado={selectedLocal.estado} />)}
+
+      {locales.map((local) => (
+        <ListadoLocales
+          key={local.id}
+          {...local}
+          onSelect={(id) => setSelectedId(id)}
+          selectedId={selectedId}
+        />
+      ))}
+    </>
   )
 }
