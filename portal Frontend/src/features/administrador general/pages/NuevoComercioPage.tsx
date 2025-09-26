21c7c78e91531781603f6265480c3a4fe34b70df
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-26 14:58:54 -0300

    Update NuevoComercioPage.tsx

--- portal Frontend/src/features/administrador general/pages/NuevoComercioPage.tsx
+++ portal Frontend/src/features/administrador general/pages/NuevoComercioPage.tsx
@@ -1,5 +1,17 @@
+import { Title } from "../../../shared/components/Title"
+import FormIngresoAdminGeneral from "../components/FormIngresoAdminGeneral"
+import { CampoComercio, type FormData } from "../types/PropsIngresosNuevosAdminGeneral"
+
 export const NuevoComercioPage = () => {
+    const handleSave = async (data: FormData) => {
+    //aca van los envios de datos al backend!
+    alert(JSON.stringify(data, null, 2));
+};
+    
     return (
-        <div>NuevoComercioPage</div>
+        <>
+        <Title text="Nuevo Comercio"/>
+        <FormIngresoAdminGeneral tipoCampos={CampoComercio} onSubmit={handleSave} />
+        </>
     )
 }
