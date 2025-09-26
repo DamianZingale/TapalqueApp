668111d73200737bc05e29c2c0cbe3a414d8e23f
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-26 14:58:57 -0300

    Update NuevoGastronomicoPage.tsx

--- portal Frontend/src/features/administrador general/pages/NuevoGastronomicoPage.tsx
+++ portal Frontend/src/features/administrador general/pages/NuevoGastronomicoPage.tsx
@@ -1,5 +1,17 @@
+import { Title } from "../../../shared/components/Title"
+import { CamposGastronomico, type FormData } from "../types/PropsIngresosNuevosAdminGeneral"
+import FormIngresoAdminGeneral from "../components/FormIngresoAdminGeneral"
+
+const handleSave = async (data: FormData) => {
+    //aca van los envios de datos al backend!
+    alert(JSON.stringify(data, null, 2));
+};
+
 export const NuevoGastronomicoPage = () => {
     return (
-        <div>NuevoGastronomicoPage</div>
+        <>
+            <Title text="Nuevo local gastronomico" />
+            <FormIngresoAdminGeneral tipoCampos={CamposGastronomico} onSubmit={handleSave} />
+        </>
     )
 }
