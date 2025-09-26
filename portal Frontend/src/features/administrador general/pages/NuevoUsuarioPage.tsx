9266a16fa59f727a9556e97a7f202482c96133be
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-26 14:59:05 -0300

    Update NuevoUsuarioPage.tsx

--- portal Frontend/src/features/administrador general/pages/NuevoUsuarioPage.tsx
+++ portal Frontend/src/features/administrador general/pages/NuevoUsuarioPage.tsx
@@ -1,5 +1,17 @@
+import { Title } from "../../../shared/components/Title"
+import FormIngresoAdminGeneral from "../components/FormIngresoAdminGeneral"
+import { CampoUsuario } from "../types/PropsIngresosNuevosAdminGeneral"
+import { type FormData } from "../types/PropsIngresosNuevosAdminGeneral"
+
+const handleSave = async (data: FormData) => {
+    //aca van los envios de datos al backend!
+    alert(JSON.stringify(data, null, 2));
+}
 export const NuevoUsuarioPage = () => {
     return (
-        <div>NuevoUsuarioPage</div>
+        <>
+            <Title text="Nuevo Usuario" />
+            <FormIngresoAdminGeneral tipoCampos={CampoUsuario} onSubmit={handleSave} />
+        </>
     )
 }
