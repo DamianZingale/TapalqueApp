e39c94ea284df5bce896c3268b6c16c4971228fa
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-26 14:58:02 -0300

    Update ListadoLocales.tsx

--- portal Frontend/src/features/administrador general/components/ListadoLocales.tsx
+++ portal Frontend/src/features/administrador general/components/ListadoLocales.tsx
@@ -1,6 +1,6 @@
-import type { PropsListadoLocales } from "../../../shared/types/PropsAdminGeneral";
+import type { PropsListadoLocalesUsuarios } from "../../../shared/types/PropsAdminGeneral";
 
-export const ListadoLocales: React.FC<PropsListadoLocales> = ({ id, estado, nombre, direccion, onSelect, selectedId }) => {
+export const ListadoLocalesUsuarios: React.FC<PropsListadoLocalesUsuarios> = ({ id, estado, nombre, direccionOtipo, onSelect, selectedId }) => {
     return (
         <div
             className={`card text-center mb-4 cursor-pointer ${selectedId === id ? 'border-secondary' : ''}`}
@@ -10,7 +10,7 @@ export const ListadoLocales: React.FC<PropsListadoLocales> = ({ id, estado, nomb
             <div className="card-body">
                 <h5 className="card-title">{nombre}</h5>
             </div>
-            <div className="card-footer text-body-secondary">{direccion}</div>
+            <div className="card-footer text-body-secondary">{direccionOtipo}</div>
         </div>
     );
 }
