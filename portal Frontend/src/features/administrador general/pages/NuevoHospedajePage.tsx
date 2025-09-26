83d736c9ee16dade50a3bbf0786de7a7b07734df
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-26 14:59:00 -0300

    Update NuevoHospedajePage.tsx

--- portal Frontend/src/features/administrador general/pages/NuevoHospedajePage.tsx
+++ portal Frontend/src/features/administrador general/pages/NuevoHospedajePage.tsx
@@ -1,5 +1,18 @@
+import { Title } from "../../../shared/components/Title"
+import { CampoHospedaje, type FormData} from "../types/PropsIngresosNuevosAdminGeneral"
+import FormIngresoAdminGeneral from "../components/FormIngresoAdminGeneral"
+
 export const NuevoHospedajePage = () => {
+
+    const handleSave = async (data: FormData) => {
+        //aca van los envios de datos al backend!
+        alert(JSON.stringify(data, null, 2));
+    };
+    
     return (
-        <div>NuevoHospedajePage</div>
+        <>
+            <Title text="Nuevo Hospedaje"/>
+            <FormIngresoAdminGeneral tipoCampos={CampoHospedaje} onSubmit={handleSave}/>
+        </>
     )
 }
