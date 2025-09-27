b3c808348172b9c53179aca893d9d3f36c487406
Santiago Lamot <santiagolamot@hotmail.com>
2025-09-27 11:53:59 -0300

    Update BotonesAccionAdmin.tsx

--- portal Frontend/src/features/administrador general/components/BotonesAccionAdmin.tsx
+++ portal Frontend/src/features/administrador general/components/BotonesAccionAdmin.tsx
@@ -10,14 +10,20 @@ export const BotonesAccionAdmin: React.FC<BotonesAccionProps> = ({ id, estado })
             state: { id } // acá pasás el id sin mostrarlo en la URL
         });
     }
+    const handleActivarDesacticar =()=>{
+        alert("Llamar al backend con la peticion delete")
+    }
+    const handleEliminar = () =>{
+        alert("Llamar al backend con la peticion update para cambiar el estado")
+    }
 
     return (
         <div className="d-flex gap-2 justify-content-center mb-3">
-            <button className={`btn ${estado === "Activo" ? "btn-warning" : "btn-success"}`}>
+            <button className={`btn ${estado === "Activo" ? "btn-warning" : "btn-success"}`} onClick={handleActivarDesacticar}>
                 {estado === "Activo" ? "Desactivar" : "Activar"}
             </button>
             <button onClick={handleClickEditar} className="btn btn-primary">Editar</button>
-            <button className="btn btn-danger">Eliminar</button>
+            <button className="btn btn-danger" onClick={handleEliminar}>Eliminar</button>
         </div>
     )
 }
