189c27677e0bd10b34bdddefee3ad99d200f05e4
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 19:33:04 -0300

    Update ComercioService.java

--- portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/service/ComercioService.java
+++ portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/service/ComercioService.java
@@ -1,12 +1,36 @@
 package com.tapalque.comercio.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tapalque.comercio.dto.ComercioRequestDTO;
+import com.tapalque.comercio.dto.ComercioResponseDTO;
 import com.tapalque.comercio.repository.ComercioRepository;
 
 @Service
 public class ComercioService {
     @Autowired
     private ComercioRepository comercioRepository;
+
+    public ComercioResponseDTO crear(ComercioRequestDTO dto) {
+        throw new UnsupportedOperationException("Unimplemented method 'crear'");
+    }
+
+    public List<ComercioResponseDTO> obtenerTodos() {
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerTodos'");
+    }
+
+    public ComercioResponseDTO obtenerPorId(Long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerPorId'");
+    }
+
+    public ComercioResponseDTO actualizar(Long id, ComercioRequestDTO dto) {
+        throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
+    }
+
+    public void eliminar(Long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
 }
