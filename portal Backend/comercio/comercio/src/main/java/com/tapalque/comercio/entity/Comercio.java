8746487bb56808f4a146b925aafe1e2d28685e2e
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 16:27:03 -0300

    Update Comercio.java

--- portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/entity/Comercio.java
+++ portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/entity/Comercio.java
@@ -1,3 +1,47 @@
 package com.tapalque.comercio.entity;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+@Table(name = "comercio")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Comercio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descripcion;
+
+    private String direccion;
+
+    private String horario;
+
+    private String telefono;
+
+    private String urlMap;
+
+    private String Facebook;
+
+    private String Instagram;
+
+    @OneToMany(mappedBy = "comercio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ComercioImagen> imagenes = new ArrayList<>();
 }
