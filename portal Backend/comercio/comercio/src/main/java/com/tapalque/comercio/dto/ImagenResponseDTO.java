9ae0561889a6627f5fc2b6a611f47ab361cdea05
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 17:57:04 -0300

    Create ImagenResponseDTO.java

---
+++ portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/dto/ImagenResponseDTO.java
@@ -0,0 +1,13 @@
+package com.tapalque.comercio.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class ImagenResponseDTO {
+    Long id;
+    String imagenUrl;
+}
