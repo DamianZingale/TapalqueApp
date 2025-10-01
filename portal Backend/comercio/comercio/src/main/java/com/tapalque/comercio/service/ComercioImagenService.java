13772f3b6f951898250d515431a9e98e6e615eb7
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 16:42:59 -0300

    Create ComercioImagenService.java

---
+++ portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/service/ComercioImagenService.java
@@ -0,0 +1,10 @@
+package com.tapalque.comercio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tapalque.comercio.repository.ComercioImagenRepository;
+
+public class ComercioImagenService {
+    @Autowired
+    private ComercioImagenRepository cImagenRepository;
+}
