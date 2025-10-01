45bc20dca3072c9b848ab966796633eeb8fe9d8d
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 17:57:09 -0300

    Create ComercioResponseDTO.java

---
+++ portal Backend/comercio/comercio/src/main/java/com/tapalque/comercio/dto/ComercioResponseDTO.java
@@ -0,0 +1,30 @@
+package com.tapalque.comercio.dto;
+
+import java.util.List;
+
+import com.tapalque.comercio.entity.Comercio;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class ComercioResponseDTO {
+    private Long id;
+    private String titulo;
+    private String description;
+    private String direccion;
+    private String horario;
+    private String telefono;
+    private String urlMap;
+    private String facebook;
+    private String instagram;
+    private List<String> imagenes;
+
+    //constructor para matear de entity a dto
+    public ComercioResponseDTO(Comercio c){
+        //Falta desarrollar
+    }
+}
