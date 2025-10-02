cface764d723071f7368777ac0fa93fb3890a1c1
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 21:39:12 -0300

    Update ComercioResponseDTO.java

--- portal Backend/comercio/src/main/java/com/tapalque/comercio/dto/ComercioResponseDTO.java
+++ portal Backend/comercio/src/main/java/com/tapalque/comercio/dto/ComercioResponseDTO.java
@@ -1,6 +1,7 @@
 package com.tapalque.comercio.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.tapalque.comercio.entity.Comercio;
 
@@ -21,10 +22,22 @@ public class ComercioResponseDTO {
     private String urlMap;
     private String facebook;
     private String instagram;
-    private List<String> imagenes;
+    private List<ImagenResponseDTO> imagenes;
 
-    //constructor para matear de entity a dto
-    public ComercioResponseDTO(Comercio c){
-        //Falta desarrollar
+    // constructor para matear de entity a dto
+    public ComercioResponseDTO(Comercio c) {
+        this.id = c.getId();
+        this.titulo = c.getTitulo();
+        this.description = c.getDescripcion();
+        this.direccion = c.getDireccion();
+        this.horario = c.getHorario();
+        this.telefono = c.getTelefono();
+        this.urlMap = c.getUrlMap();
+        this.facebook = c.getFacebook();
+        this.instagram = c.getInstagram();
+        this.imagenes = c.getImagenes()
+                .stream()
+                .map(img -> new ImagenResponseDTO(img.getId(), img.getImagenUrl()))
+                .collect(Collectors.toList());
     }
 }
