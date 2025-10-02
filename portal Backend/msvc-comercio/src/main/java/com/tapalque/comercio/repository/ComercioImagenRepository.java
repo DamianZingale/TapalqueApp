83e9e31e3f606632d55efadf2868682949ea413f
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-02 17:35:23 -0300

    Update ComercioImagenRepository.java

--- portal Backend/msvc-comercio/src/main/java/com/tapalque/comercio/repository/ComercioImagenRepository.java
+++ portal Backend/msvc-comercio/src/main/java/com/tapalque/comercio/repository/ComercioImagenRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tapalque.comercio.entity.Comercio;
 import com.tapalque.comercio.entity.ComercioImagen;
 
 @Repository
