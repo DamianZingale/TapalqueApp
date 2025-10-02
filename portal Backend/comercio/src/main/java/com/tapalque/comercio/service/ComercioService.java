f0d5f22bb8e1696605a98bd8b78039a1f5958e0e
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 21:39:24 -0300

    Update ComercioService.java

--- portal Backend/comercio/src/main/java/com/tapalque/comercio/service/ComercioService.java
+++ portal Backend/comercio/src/main/java/com/tapalque/comercio/service/ComercioService.java
@@ -1,12 +1,15 @@
 package com.tapalque.comercio.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tapalque.comercio.Exceptions.ComercioNotFoundException;
 import com.tapalque.comercio.dto.ComercioRequestDTO;
 import com.tapalque.comercio.dto.ComercioResponseDTO;
+import com.tapalque.comercio.entity.Comercio;
 import com.tapalque.comercio.repository.ComercioRepository;
 
 @Service
@@ -19,18 +22,38 @@ public class ComercioService {
     }
 
     public List<ComercioResponseDTO> obtenerTodos() {
-        throw new UnsupportedOperationException("Unimplemented method 'obtenerTodos'");
+        return comercioRepository.findAll()
+                .stream()
+                .map(ComercioResponseDTO::new)
+                .collect(Collectors.toList());
     }
 
     public ComercioResponseDTO obtenerPorId(Long id) {
-        throw new UnsupportedOperationException("Unimplemented method 'obtenerPorId'");
+        Comercio comercio = comercioRepository.findById(id)
+                .orElseThrow(() -> new ComercioNotFoundException(id));
+        return new ComercioResponseDTO(comercio);
     }
 
-    public ComercioResponseDTO actualizar(Long id, ComercioRequestDTO dto) {
-        throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
+    public ComercioResponseDTO actualizarCompleto(Long id, ComercioRequestDTO dto) {
+        Comercio comercio = comercioRepository.findById(id)
+                .orElseThrow(() -> new ComercioNotFoundException(id));
+        Comercio actualizado = new Comercio(dto);
+        actualizado.setId(comercio.getId());
+        comercioRepository.save(actualizado);
+        return new ComercioResponseDTO(actualizado);
+    }
+
+    public ComercioResponseDTO actualizarParcial(Long id, ComercioRequestDTO dto) {
+        Comercio comercio = comercioRepository.findById(id)
+                .orElseThrow(() -> new ComercioNotFoundException(id));
+        comercio.actualizarParcial(dto);
+        comercioRepository.save(comercio);
+        return new ComercioResponseDTO(comercio);
     }
 
     public void eliminar(Long id) {
-        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+        Comercio comercio = comercioRepository.findById(id)
+                .orElseThrow(() -> new ComercioNotFoundException(id));
+        comercioRepository.delete(comercio);
     }
 }
