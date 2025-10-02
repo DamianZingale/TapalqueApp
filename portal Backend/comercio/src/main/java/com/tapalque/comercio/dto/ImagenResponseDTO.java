4479b9a8db4eea164f4e76f9745a65267d4807cb
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 21:39:08 -0300

    Update controller methods and add AllArgsConstructor

--- portal Backend/comercio/src/main/java/com/tapalque/comercio/dto/ImagenResponseDTO.java
+++ portal Backend/comercio/src/main/java/com/tapalque/comercio/dto/ImagenResponseDTO.java
@@ -1,9 +1,11 @@
 package com.tapalque.comercio.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
