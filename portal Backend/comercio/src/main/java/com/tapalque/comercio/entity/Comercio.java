8d58c681157607bdab6c6c10c18ddfd8ea41e223
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-01 21:39:17 -0300

    Update Comercio.java

--- portal Backend/comercio/src/main/java/com/tapalque/comercio/entity/Comercio.java
+++ portal Backend/comercio/src/main/java/com/tapalque/comercio/entity/Comercio.java
@@ -1,6 +1,9 @@
 package com.tapalque.comercio.entity;
 
 import java.util.List;
+
+import com.tapalque.comercio.dto.ComercioRequestDTO;
+
 import java.util.ArrayList;
 
 import jakarta.persistence.CascadeType;
@@ -44,4 +47,34 @@ public class Comercio {
 
     @OneToMany(mappedBy = "comercio", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ComercioImagen> imagenes = new ArrayList<>();
+
+    public Comercio(ComercioRequestDTO dto) {
+        this.setTitulo(dto.getTitulo());
+        this.setDescripcion(dto.getDescripcion());
+        this.setDireccion(dto.getDireccion());
+        this.setHorario(dto.getHorario());
+        this.setTelefono(dto.getTelefono());
+        this.setUrlMap(dto.getUrlMap());
+        this.setFacebook(dto.getFacebook());
+        this.setInstagram(dto.getInstagram());
+    }
+
+    public void actualizarParcial(ComercioRequestDTO dto) {
+        if (dto.getTitulo() != null)
+            this.titulo = dto.getTitulo();
+        if (dto.getDescripcion() != null)
+            this.descripcion = dto.getDescripcion();
+        if (dto.getDireccion() != null)
+            this.direccion = dto.getDireccion();
+        if (dto.getHorario() != null)
+            this.horario = dto.getHorario();
+        if (dto.getTelefono() != null)
+            this.telefono = dto.getTelefono();
+        if (dto.getUrlMap() != null)
+            this.urlMap = dto.getUrlMap();
+        if (dto.getFacebook() != null)
+            this.facebook = dto.getFacebook();
+        if (dto.getInstagram() != null)
+            this.instagram = dto.getInstagram();
+    }
 }
