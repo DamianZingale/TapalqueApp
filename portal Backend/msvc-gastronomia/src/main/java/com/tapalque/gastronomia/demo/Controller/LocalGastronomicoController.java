2b95efa3907bfe981a6acd9825055a9447b83bc8
DamianZingale <damianzingale@gmail.com>
2025-10-05 18:53:13 -0300

    Set database username and password to 'root' for development environment

--- portal Backend/msvc-gastronomia/src/main/java/com/tapalque/gastronomia/demo/Controller/LocalGastronomicoController.java
+++ portal Backend/msvc-gastronomia/src/main/java/com/tapalque/gastronomia/demo/Controller/LocalGastronomicoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tapalque.gastronomia.demo.DTO.RestaurantDTO;
@@ -21,7 +20,7 @@ import com.tapalque.gastronomia.demo.Service.I_RestaurantService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/gastronomia/local")
+//@RequestMapping("/api/gastronomia/local")
 @CrossOrigin("*")
 public class LocalGastronomicoController {
 
