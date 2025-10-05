743068efec9d80c886e4cced54945afb8a9dc42f
DamianZingale <damianzingale@gmail.com>
2025-10-05 11:51:20 -0300

    Configured dynamic ports and integrated services with Eureka and Gateway
    - Set dynamic ports for all microservices
    - Registered services in Eureka
    - Configured Gateway routing
    - Renamed main classes from DemoApplication

---
+++ portal Backend/msvc-hosteleria/src/main/java/com/tapalque/hosteleria/demo/HosteleriaApplication.java
@@ -0,0 +1,13 @@
+package com.tapalque.hosteleria.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosteleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosteleriaApplication.class, args);
+	}
+
+}
