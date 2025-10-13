c71ffaf536daece780dc28f5edf51caa20c882e8
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-13 15:58:08 -0300

    Add initial Spring Boot JWT microservice project

---
+++ portal Backend/msvc-jwt/src/test/java/com/tapalque/msvc_jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapalque.msvc_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
