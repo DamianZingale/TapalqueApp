8f3a89109d831331a1ebc413c84ee3cd2668cb4b
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-20 18:56:09 -0300

    Create UserResponseDTO.java

---
+++ portal Backend/msvc-jwt/src/main/java/com/tapalque/jwt/dto/UserResponseDTO.java
@@ -0,0 +1,16 @@
+package com.tapalque.jwt.dto;
+
+import com.tapalque.jwt.enu.RolName;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserResponseDTO {
+    private String email;
+    private String firtName;
+    private String lastName;
+    private String nameEmprise;
+    private RolName rol;
+}
