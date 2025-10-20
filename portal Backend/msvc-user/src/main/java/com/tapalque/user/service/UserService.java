75e7a3ada58cc038738f4cdee79a14a2f4c5c4bf
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-20 18:56:24 -0300

    Update UserService.java

--- portal Backend/msvc-user/src/main/java/com/tapalque/user/service/UserService.java
+++ portal Backend/msvc-user/src/main/java/com/tapalque/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.tapalque.user.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.tapalque.user.dto.UserRequestDTO;
+import com.tapalque.user.dto.UserResponseDTO;
 import com.tapalque.user.entity.Role;
 import com.tapalque.user.entity.User;
 import com.tapalque.user.enu.RolName;
@@ -14,27 +17,46 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepo;
     private final RoleRepository roleRepo;
 
-    public User registrarUsuario(UserRequestDTO dto, RolName rolNombre) {
-        Role rol = roleRepo.findByNombre(rolNombre)
-                .orElseThrow(() -> new RuntimeException("Rol no encontrado"));
+    public UserResponseDTO register(UserRequestDTO dto) {
+        if (userRepo.findByEmail(dto.getCorreo()).isPresent()) {
+            throw new IllegalArgumentException("El correo ya está registrado");
+        }
 
-        User usuario = User.builder()
+        Role role = roleRepo.findByName(RolName.USER_GRAL)
+                .orElseThrow(() -> new RuntimeException("Rol USER_GRAL no encontrado"));
+
+        User user = User.builder()
                 .email(dto.getCorreo())
-                .password(dto.getContrasena()) // encripta en MSVC JWT
+                .password(dto.getContrasena()) // encripta desde MSVC jwt
                 .firtName(dto.getNombre())
                 .lastName(dto.getApellido())
                 .nameEmprise(dto.getEmpresa())
-                .role(rol)
+                .registrationDate(LocalDateTime.now())
+                .role(role)
                 .build();
 
-        return userRepo.save(usuario);
+        userRepo.save(user);
+
+        return mapToDTO(user);
     }
 
-    public User getByEmail(String email) {
-        return userRepo.findByEmail(email)
+    public UserResponseDTO getByEmail(String email) {
+        User user = userRepo.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        return mapToDTO(user);
+    }
+
+    private UserResponseDTO mapToDTO(User user) {
+        return UserResponseDTO.builder()
+                .email(user.getEmail())
+                .firtName(user.getFirtName())
+                .lastName(user.getLastName())
+                .nameEmprise(user.getNameEmprise())
+                .rol(user.getRole().getName())
+                .build();
     }
 }
