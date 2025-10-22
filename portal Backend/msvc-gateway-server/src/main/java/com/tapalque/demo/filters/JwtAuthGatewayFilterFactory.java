f580821152d704825d9d612782f6dec3ba4fc288
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-22 19:52:27 -0300

    Refactor gateway server JWT filter and config

---
+++ portal Backend/msvc-gateway-server/src/main/java/com/tapalque/demo/filters/JwtAuthGatewayFilterFactory.java
@@ -0,0 +1,44 @@
+package com.tapalque.demo.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<JwtAuthGatewayFilterFactory.Config> {
+
+    public JwtAuthGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String path = exchange.getRequest().getURI().getPath();
+            System.out.println("El Path que llega al gatewat Filter: "+path);
+
+            if (path.startsWith("/api/user/register") ||
+                path.startsWith("/api/user/exists") ||
+                path.startsWith("/api/jwt/") ||
+                path.startsWith("/api/public")||
+                path.startsWith("/api/user/email/")) {
+                    System.out.println("Entra a este IF!");
+                return chain.filter(exchange);
+            }
+
+            String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+    }
+}
