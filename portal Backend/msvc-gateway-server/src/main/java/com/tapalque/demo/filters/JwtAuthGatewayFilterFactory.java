af006afc1c5684753649e0afb7c93b271b87c2a7
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-23 16:21:24 -0300

    Update JwtAuthGatewayFilterFactory.java

--- portal Backend/msvc-gateway-server/src/main/java/com/tapalque/demo/filters/JwtAuthGatewayFilterFactory.java
+++ portal Backend/msvc-gateway-server/src/main/java/com/tapalque/demo/filters/JwtAuthGatewayFilterFactory.java
@@ -1,11 +1,11 @@
 package com.tapalque.demo.filters;
 
+import java.util.List;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
 @Component
 public class JwtAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<JwtAuthGatewayFilterFactory.Config> {
@@ -18,24 +18,30 @@ public class JwtAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<Jw
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             String path = exchange.getRequest().getURI().getPath();
-            System.out.println("El Path que llega al gatewat Filter: "+path);
 
-            if (path.startsWith("/api/user/register") ||
-                path.startsWith("/api/user/exists") ||
-                path.startsWith("/api/jwt/") ||
-                path.startsWith("/api/public")||
-                path.startsWith("/api/user/email/")) {
-                    System.out.println("Entra a IF rutas publicas!");
+            //Rutas publicas que debe permitir el gateway
+            List<String> publicPaths = List.of(
+                    "/api/user/register",
+                    "/api/user/exists",
+                    "/api/jwt/",
+                    "/api/public",
+                    "/api/user/email/");
+
+            //Si es publica permite el ingreso
+            boolean esRutaPublica = publicPaths.stream()
+                    .anyMatch(path::startsWith);
+            if (esRutaPublica) {
                 return chain.filter(exchange);
             }
 
+            //Chequea el token
             String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                System.out.println("Entra a If que rechaza");
                 return exchange.getResponse().setComplete();
             }
-            System.out.println("Autoriza el token");
+
+            //Perimte el request y continua al Microserv
             return chain.filter(exchange);
         };
     }
