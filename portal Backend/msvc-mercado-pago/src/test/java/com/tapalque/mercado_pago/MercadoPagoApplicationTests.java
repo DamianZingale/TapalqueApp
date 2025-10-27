7cfbe8bb39eac3990188486dfd85155f2c207298
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-27 19:36:43 -0300

    Initialize Mercado Pago microservice project

---
+++ portal Backend/msvc-mercado-pago/src/test/java/com/tapalque/mercado_pago/MercadoPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapalque.mercado_pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
