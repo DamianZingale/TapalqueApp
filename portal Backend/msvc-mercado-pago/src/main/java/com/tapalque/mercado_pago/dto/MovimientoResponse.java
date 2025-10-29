ba8577e979385fac3374fd0963c3f2bb0ad3ba91
Santiago Lamot <santiagolamot@hotmail.com>
2025-10-29 16:45:53 -0300

    Create MovimientoResponse.java

---
+++ portal Backend/msvc-mercado-pago/src/main/java/com/tapalque/mercado_pago/dto/MovimientoResponse.java
@@ -0,0 +1,20 @@
+package com.tapalque.mercado_pago.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tapalque.mercado_pago.entity.Movimiento;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MovimientoResponse {
+    @JsonProperty("results")
+    private List<Movimiento> results;
+}
