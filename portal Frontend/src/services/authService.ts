b88b1360910dbded8b1ac95be5f63a6fdd73f525
Nahuel C. <nahuelncejas@gmail.com>
2025-11-11 19:52:35 -0300

    Piripipi
    Token para el login
    "Buenas noches, che nahuel primero deberías empezar por el login a probar, porque eso te devuelve el jwt que se guarda en local storage y con ese jwt ya haces los otros request mas seguro, porque varios son protegidos para que se este logueado"

---
+++ portal Frontend/src/services/authService.ts
@@ -0,0 +1,19 @@
+
+const TOKEN_KEY = "token";
+
+export function saveToken(token: string): void {
+    localStorage.setItem(TOKEN_KEY, token);
+}
+
+export function getToken(): string | null {
+    return localStorage.getItem(TOKEN_KEY);
+}
+
+export function removeToken(): void {
+    localStorage.removeItem(TOKEN_KEY);
+}
+
+export function isLoggedIn(): boolean {
+    const token = getToken();
+    return !!token;
+}
