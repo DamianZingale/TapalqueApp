eb2c2ec3d5b068cb1f25dc4624cbd8a303e5fc64
DamianZingale <damianzingale@gmail.com>
2025-11-20 16:51:40 -0300

    fix: eureka-server pom.xml add actuator && add healthcheck in docker-compose.yml

--- portal Backend/msvc-gastronomia/src/main/java/com/tapalque/gastronomia/demo/Controller/LocalGastronomicoController.java
+++ portal Backend/msvc-gastronomia/src/main/java/com/tapalque/gastronomia/demo/Controller/LocalGastronomicoController.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tapalque.gastronomia.demo.GastronomiaApplication;
 import com.tapalque.gastronomia.demo.DTO.RestaurantDTO;
+import com.tapalque.gastronomia.demo.GastronomiaApplication;
 import com.tapalque.gastronomia.demo.Service.I_RestaurantService;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -20,7 +20,7 @@ import jakarta.validation.Valid;
 
 @RestController
 
-@CrossOrigin("*")
+@RequestMapping("/api/gastronomia")
 public class LocalGastronomicoController {
 
    private final I_RestaurantService localGastronomicoService;
